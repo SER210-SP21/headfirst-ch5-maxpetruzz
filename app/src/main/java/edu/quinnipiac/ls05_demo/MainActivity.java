f0b0bc3889a4845a6e7d90b0a257c62a742c78b3
maxpetruzziello <mlpetruzziello@qu.edu>
2021-02-11 14:46:48 -0500

    LS05 Frame Layout Demo

---
+++ app/src/main/java/edu/quinnipiac/ls05_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.quinnipiac.ls05_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
